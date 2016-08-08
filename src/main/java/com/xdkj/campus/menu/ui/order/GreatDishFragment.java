20664bfcf4e1205646c8b1a822bb077e617f3af6
arilpan <1045375318@qq.com>
2016-08-08 17:44:36 +0800

    error ui with waterfall flow ,other ok,add select mall  function

---
+++ src/main/java/com/xdkj/campus/menu/ui/order/GreatDishFragment.java
@@ -0,0 +1,52 @@
+package com.xdkj.campus.menu.ui.order;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.xdkj.campus.menu.R;
+import com.xdkj.campus.menu.base.BaseFragment;
+
+
+public class CancleOrderFragment extends BaseFragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+
+//    private OnFragmentInteractionListener mListener;
+
+    public CancleOrderFragment() {
+        // Required empty public constructor
+    }
+
+    public static CancleOrderFragment newInstance() {
+
+        Bundle args = new Bundle();
+        CancleOrderFragment fragment = new CancleOrderFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_order_cancel, container, false);
+        initView(view);
+        return view;
+    }
+
+
+    private void initView(View view) {
+    }
+
+    @Override
+    public boolean onBackPressedSupport() {
+        // 这里实际项目中推荐使用 EventBus接耦
+//        ((TestOne) getParentFragment()).onBackToFirstFragment();
+        Log.e("arilpan", "on back press");
+        return false;
+//        return true;
+    }
+}
